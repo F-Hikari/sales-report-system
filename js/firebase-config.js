8089bc55e326784f54ec325c4c3f90b77f25173f
F-Hikari <seaqueen203@gmail.com>
2025-03-11 00:09:53 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,23 +1,11 @@
 // firebase-config.js - Firebase設定
 const firebaseConfig = {
-    // 実際のFirebaseプロジェクト設定に置き換える
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
   apiKey: "AIzaSyBqR9NdUvXcuhgyj95L6x8M9uelIc1wJto",
   authDomain: "sales-report-system.firebaseapp.com",
   projectId: "sales-report-system",
-  storageBucket: "sales-report-system.firebasestorage.app",
+  storageBucket: "sales-report-system.appspot.com", // 注意: これは.firebasestorage.appではなく.appspot.comになるはずです
   messagingSenderId: "149432948067",
   appId: "1:149432948067:web:271301f844c6f2b0286f56"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 };
 
 // Firebaseの初期化
